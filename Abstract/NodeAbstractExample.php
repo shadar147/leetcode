c380c2060f3c3711cf3fb04f1988afbb35bcae44
Lebedev Vadim <v.lebedev@qsoft.ru>
2022-12-27 16:47:41 +0800

    Solution for ex 589

---
+++ Abstract/NodeAbstractExample.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shadar\Leetcode\Abstract;
+
+use Shadar\Leetcode\Entities\Node;
+
+abstract class NodeAbstractExample extends AbstractExample
+{
+    protected function createRootNode(array $nodesList): ?Node
+    {
+        // TODO:: make an array to nodes parser
+
+        return null;
+    }
+}
